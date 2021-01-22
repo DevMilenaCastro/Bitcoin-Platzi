28126197317dccf361433601b6176ad9e39ac4ac
milenac <ingmilenacastro@hotmail.com>
2021-01-21 23:57:53 -0500

    Project mobile firt course project

---
+++ js/App.js
@@ -0,0 +1,23 @@
+const navSlide = () => {
+  const lines = document.querySelector(".lines");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
+  // Toggle Nav
+  lines.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+
+    //   Animate Links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards 
+            ${index / 7 + 0.5}s`;
+      }
+    });
+    //Lines animation
+    lines.classList.toggle("toggle");
+  });
+};
+
+navSlide();
